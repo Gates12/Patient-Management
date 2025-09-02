1e6c47a7c05a4bd1bdf23b816f354cefff7d9fa3
Gates Babu <gates@Gatess-MacBook-Air.local>
2025-09-02 23:34:34 +0530

    POST End Point

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,13 +1,14 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import com.pm.patientservice.service.PatientService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +16,21 @@ import java.util.List;
 @RequestMapping("/patients")
 public class PatientController {
     private final PatientService patientService;
+
     public PatientController(PatientService patientService) {
         this.patientService = patientService;
     }
 
     @GetMapping
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
-        List<PatientResponseDTO>patients=patientService.getPatients();
+        List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
     }
 }
